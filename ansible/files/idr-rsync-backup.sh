76cd035d5a4470af13fd1e8f725efefa74e9601f
jmoore <josh@glencoesoftware.com>
2017-03-16 16:57:07 +0100

    Further rsync fixes

--- ansible/files/idr-rsync-backup.sh
+++ ansible/files/idr-rsync-backup.sh
@@ -1,19 +1,10 @@
-The directories contained in this directory are
-dumps of the IDR database produced using:
-
- $ pg_dump -Fd -j 8 -f ./$DATE ...
-
-which allows restoring the database in parallel:
-
- $ pg_restore -Fd -j 8 -d idr ./$DATE
-[centos@demo32-omero omero-sql]$ cat backup.sh
 #!//usr/bin/env bash
 
 set -e
 set -u
 
-PATH=$PATH:/home/omero/OMERO.server/bin
-HOST=$(omero config get omero.db.host)
+HOST=$(su omero -lc "omero config get omero.db.host")
+echo $HOST
 DATE=$(date +"%Y-%m-%d")
 test -e $DATE && {
   echo $DATE already exists
@@ -22,6 +13,6 @@ test -e $DATE && {
 time pg_dump -h $HOST -U omero idr -j 8 -Fd -f $DATE \
     --exclude-table-data password \
     --exclude-table-data eventlog
-sudo chmod -R a+rX $DATE
+chmod -R a+rX $DATE
 rm -f latest
 ln -s $DATE latest

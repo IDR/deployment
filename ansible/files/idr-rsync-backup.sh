2bbcdc426716bccf257ddc22b5e48d4cbf06193b
jmoore <josh@glencoesoftware.com>
2017-04-19 07:53:39 +0200

    rsync: use PG environ variables

--- ansible/files/idr-rsync-backup.sh
+++ ansible/files/idr-rsync-backup.sh
@@ -3,14 +3,14 @@
 set -e
 set -u
 
-HOST=$(su omero -lc "/opt/omero/server/OMERO.server/bin/omero config get omero.db.host")
-PASS=$(su omero -lc "/opt/omero/server/OMERO.server/bin/omero config get omero.db.pass")
+PGHOST=$(su omero -lc "/opt/omero/server/OMERO.server/bin/omero config get omero.db.host")
+PGPASSWORD=$(su omero -lc "/opt/omero/server/OMERO.server/bin/omero config get omero.db.pass")
 DATE=$(date +"%Y-%m-%d")
 test -e $DATE && {
   echo $DATE already exists
   exit 1
 }
-time pg_dump -h $HOST -U omero idr -j 8 -Fd -f $DATE \
+time pg_dump -U omero idr -j 8 -Fd -f $DATE \
     --exclude-table-data password \
     --exclude-table-data eventlog
 chmod -R a+rX $DATE

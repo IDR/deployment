fe5d12d9b2e101f9d483ecb1ddd6c78347c60cac
jmoore <josh@glencoesoftware.com>
2017-05-08 17:11:51 +0200

    Deployment fixes (rsync/mineotaur)

--- ansible/files/idr-rsync-backup.sh
+++ ansible/files/idr-rsync-backup.sh
@@ -3,14 +3,14 @@
 set -e
 set -u
 
-PGHOST=$(su omero -lc "/opt/omero/server/OMERO.server/bin/omero config get omero.db.host")
-PGPASSWORD=$(su omero -lc "/opt/omero/server/OMERO.server/bin/omero config get omero.db.pass")
+export PGHOST=$(su omero-server -lc "/opt/omero/server/OMERO.server/bin/omero config get omero.db.host")
+export PGPASSWORD=$(su omero-server -lc "/opt/omero/server/OMERO.server/bin/omero config get omero.db.pass")
 DATE=$(date +"%Y-%m-%d")
 test -e $DATE && {
   echo $DATE already exists
   exit 1
 }
-time pg_dump -U omero idr -j 8 -Fd -f $DATE \
+time pg_dump -h $PGHOST -U omero idr -j 8 -Fd -f $DATE \
     --exclude-table-data password \
     --exclude-table-data eventlog
 chmod -R a+rX $DATE

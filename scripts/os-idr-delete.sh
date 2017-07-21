9f427a8ad1407374389f57b8389598cc63ddb506
Simon Li <spli@dundee.ac.uk>
2017-07-21 11:04:42 +0100

    Update delete script with omero cluster and flat net

--- scripts/os-idr-delete.sh
+++ scripts/os-idr-delete.sh
@@ -6,42 +6,41 @@
 set -eu
 
 idr_environment=$1
-for server in proxy omero database management; do
+
+for server in \
+    proxy \
+    omeroreadonly-1 \
+    omeroreadonly-2 \
+    omeroreadwrite \
+    database \
+    dockermanager \
+    dockerworker-1 \
+    dockerworker-2 \
+    management \
+    ; do
     echo openstack server delete ${idr_environment}-${server}
 done
 echo
 
-for server in omero database dockermanager dockerworker; do
-    echo openstack server delete ${idr_environment}-a-${server}
-done
-echo
-
-for volume in proxy-nginxcache omero-data database-db; do
+for volume in \
+    proxy-nginxcache \
+    omeroreadwrite-data \
+    database-db \
+    dockermanager-data \
+    ; do
     echo openstack volume delete ${idr_environment}-${volume}
 done
 echo
 
-for volume in omero-data database-db dockermanager-data; do
-    echo openstack volume delete ${idr_environment}-a-${volume}
-done
+echo "for port in \$(openstack port list --router ${idr_environment}-router -f value | cut -d\  -f1); do"
+echo "    openstack router remove port ${idr_environment}-router \$port"
+echo "done"
 echo
 
-for router in ${idr_environment}-router ${idr_environment}-a-router; do
-    echo "for port in \$(openstack port list --router ${router} -f value | cut -d\  -f1); do"
-    echo "    openstack router remove port ${router} \$port"
-    echo "done"
-    echo
-done
+echo openstack router delete ${idr_environment}-router
 echo
 
-for router in ${idr_environment}-router ${idr_environment}-a-router; do
-    echo openstack router delete ${router}
-done
-echo
-
-for network in ${idr_environment} ${idr_environment}-a; do
-    echo openstack network delete ${network}
-done
+echo openstack network delete ${idr_environment}
 echo
 
 echo openstack server list -f yaml

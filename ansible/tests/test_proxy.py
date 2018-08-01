e134062921cd143c232c920f125b3b3ded7655a5
Simon Li <spli@dundee.ac.uk>
2018-08-01 17:44:35 +0100

    Test that localhost contains some an expected HTML string

--- ansible/tests/test_proxy.py
+++ ansible/tests/test_proxy.py
@@ -14,3 +14,12 @@ def test_services_running_and_enabled(Service):
 @pytest.mark.parametrize("port", [80, 443, 9000])
 def test_omero_port_listening(Socket, port):
     assert Socket("tcp://0.0.0.0:%d" % port).is_listening
+
+
+@pytest.mark.parametrize("address", [
+    "http://localhost/",
+    "https://localhost/",
+])
+def test_html_index(Command, address):
+    out = Command.check_output('curl -kL %s' % address)
+    assert '<title>IDR: Image Data Resource</title>' in out

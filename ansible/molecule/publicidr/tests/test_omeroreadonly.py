c84eec6a1b413fa060b11ff9ec44f966641999cb
Simon Li <spli@dundee.ac.uk>
2019-06-17 18:30:33 +0100

    Only run readonly tests outside Travis
    This is due to resources constriants which mean it's not possible to run multiple OMERO servers (read-write and read-only)

--- ansible/molecule/publicidr/tests/test_omeroreadonly.py
+++ ansible/molecule/publicidr/tests/test_omeroreadonly.py
@@ -6,6 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('omeroreadonly-hosts')
 
 
+@pytest.mark.skipif(os.getenv('TRAVIS') == 'true',
+                    reason="Disabled on travis: insufficient resources")
 @pytest.mark.parametrize("name", ["omero-server", "omero-web", "nginx"])
 def test_services_running_and_enabled(host, name):
     service = host.service(name)
@@ -13,10 +15,14 @@ def test_services_running_and_enabled(host, name):
     assert service.is_enabled
 
 
+@pytest.mark.skipif(os.getenv('TRAVIS') == 'true',
+                    reason="Disabled on travis: insufficient resources")
 def test_nginx_port_listening(host):
     assert host.socket("tcp://0.0.0.0:80").is_listening
 
 
+@pytest.mark.skipif(os.getenv('TRAVIS') == 'true',
+                    reason="Disabled on travis: insufficient resources")
 @pytest.mark.parametrize("port", [4063, 4064])
 def test_omero_port_listening(host, port):
     # For some reason OMERO may listen on ipv6 instead of ipv4
@@ -24,5 +30,7 @@ def test_omero_port_listening(host, port):
             host.socket("tcp://:::%d" % port).is_listening)
 
 
+@pytest.mark.skipif(os.getenv('TRAVIS') == 'true',
+                    reason="Disabled on travis: insufficient resources")
 def test_registry_port_listening(host):
     assert host.socket("tcp://127.0.0.1:4061").is_listening
